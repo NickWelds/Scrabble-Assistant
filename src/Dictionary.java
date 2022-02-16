20c9a81f32bfed0012cbdd3afceaad6378b8efe8
Lemmonns <85978036+Lemmonns@users.noreply.github.com>
2022-02-16 11:29:23 -0500

    Created the Dictionary's loading function which returns all words under a certain word length.

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,6 +1,26 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Dictionary {
+    String filename = "Scrabble-Assistant" + File.separator + "Scrabble Words.txt";
 
-    public static void load_words(){}
-
-    public static void trim_load_words(){}
+    public ArrayList<String> getAllWordsByLength(int wordlength) {
+        ArrayList<String> wordsUnderLength = new ArrayList<>();
+        try{
+            File dictionary = new File(filename);
+            Scanner fileInput = new Scanner(dictionary);
+            while (fileInput.hasNext()) {
+                String lineInput = fileInput.nextLine();
+                if (lineInput.length() <= wordlength) {
+                    wordsUnderLength.add(lineInput);
+                    //System.out.println(lineInput);
+                }
+            }
+            fileInput.close();
+        } catch (Exception e) {
+            System.out.println("Dictionary file not found!");
+        }
+        return wordsUnderLength;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main{
   //testing this pls work
   public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("brabble");
-        System.out.println("words");
-        System.out.println("fortnite");
-        System.out.println("forge");
-        System.out.println("uhhhh");
-        System.out.println("let me know if you get this");
-    
+        Dictionary dictionary = new Dictionary();
+        dictionary.getAllWordsByLength(6);
     }
 }
